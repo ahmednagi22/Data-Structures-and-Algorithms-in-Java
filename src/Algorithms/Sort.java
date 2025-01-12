11803da5991836e9a92d17b1d34de4c5677f7ca8
Ahmed Abdelmongy <ahmednagi318@gmail.com>
2025-01-12 16:37:47 +0200

    implement insertion sort function in Sort class

---
+++ src/Algorithms/Sort.java
@@ -0,0 +1,32 @@
+package Algorithms;
+
+public class Sort {
+
+    /**
+     * Sorts an array of integers in ascending order using the Insertion Sort algorithm.
+     *
+     * @param array The array to be sorted.
+     */
+    public static void insertionSort(int[] array){
+
+        // Traverse through the array from the second element
+        for (int i = 1; i < array.length; i++) {
+            // Store the current element to be compared
+            int key = array[i];
+
+            // Initialize the index of the previous element
+            int j = i-1;
+
+            // Shift elements of the sorted portion that are greater than the key
+            // to one position ahead of their current position
+            while(j >= 0 && array[j] > key){
+             array[j+1] = array[j];
+             j--;
+            }
+
+            // Place the key in its correct position
+            array[j+1] = key;
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import Algorithms.Search;
+import Algorithms.Sort;
 
 public class Main {
     public static void main(String[] args) {
-        int [] array = {1,2,3,5,8,9,11,15};
-        System.out.println(Search.binarySearch(array,112));
+        int [] array = {4,3,7,6,8,9,2,1,1};
+        Sort.insertionSort(array);
+        for(int i :array){
+            System.out.print(i+" ");
+        }
 
 
 
