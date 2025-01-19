c78b042f960f9246038c33a768d503699e90444a
Ahmed Abdelmongy <ahmednagi318@gmail.com>
2025-01-19 21:20:17 +0200

    Immplement MyStack class

---
+++ src/DataStrucures/MyStack.java
@@ -0,0 +1,50 @@
+package DataStrucures;
+
+public class MyStack {
+    private int[] stack;
+    private int maxSize;
+    private int top;
+
+    // stack constructor
+    public MyStack(int maxSize){
+        this.maxSize = maxSize;
+        stack = new int[maxSize];
+        top = -1;
+    }
+
+    // add element in the stack
+    public void push(int value){
+        if (top == maxSize-1){
+            System.out.println("Stack overflow");
+            return;
+        }
+        top++;
+        stack[top]=value;
+        System.out.println(value + " pushed into the stack");
+    }
+    // remove element from the top of stack
+    public int pop(){
+       if (top == -1){
+           System.out.println("Stack underflow");
+           return -1;
+       }
+        int result = stack[top];
+        top--;
+        System.out.println(result + " popped from the stack");
+        return result;
+    }
+    // function to peek the top of the stack
+    public int peek(){
+        if(top ==-1) {
+            System.out.println("Stack is Empty");
+            return -1;
+        }
+        return stack[top];
+    }
+    // Method to check if the stack is empty
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 import Algorithms.Search;
 import Algorithms.Sort;
+import DataStrucures.MyStack;
 
 public class Main {
     public static void main(String[] args) {
-        int [] array = {4,3,7,6,8,9,2,1,1};
-        Sort.insertionSort(array);
-        for(int i :array){
-            System.out.print(i+" ");
-        }
-
-
+        MyStack stack = new MyStack(3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.peek());
 
     }
 }
